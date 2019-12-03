01a847809721cba1f8a70cc3d229c2b5d4fe8d63
Chris Elrod <elrodc@gmail.com>
2019-12-03 08:17:51 -0500

    Updated precompile.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,4 +1,5 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+    precompile(Tuple{InplaceDHMC.var"##s18#32",Any,Any,Any,Any,Any,Any,Any,Any})
     precompile(Tuple{typeof(InplaceDHMC.regularized_cov_block_quote),Int64,Type,Int64,Int64,Bool})
 end
